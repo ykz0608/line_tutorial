054fe03fd327b8325375b5111a5bc24e6d26cfe9
ykz0608 <yongkahzin@gmail.com>
2021-05-02 06:23:52 +0800

    add code

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def Age1(event):
     alt_text='Buttons template',
     template=ButtonsTemplate(
         title='請選擇您的年齡區間',
-        text='XXX是。。。。。。。',
+        #text='XXX是。。。。。。。',
         actions=[
             PostbackTemplateAction(
                 label='20-29',
@@ -116,7 +116,7 @@ def Age2(event):
     alt_text='Buttons template',
     template=ButtonsTemplate(
         title='請選擇您的年齡區間',
-        text='XXX是。。。。。。。',
+        #text='XXX是。。。。。。。',
         actions=[
             PostbackTemplateAction(
                 label='60-69',
@@ -395,11 +395,11 @@ def test5(event):
 def result(event):
     formula = -2.1+0.39*x[0]+0.038*x[1]-0.554*x[2]+0.848*x[3]+0.147*x[4]+0.623*x[5]-0.06*x[6]+0.267*x[7]+0.178*x[8]+0.459*x[9]
     Probability = (math.exp(formula)/(1+math.exp(formula)))*100
-    result_text = '您獲得五十肩的風險機率的分數是{p}%，'.format(p=Probability)
-    if Probability >= 49:
+    result_text = '您獲得五十肩的風險機率是{p}%，'.format(p=Probability)
+    if Probability >= 50:
         message = TextSendMessage(result_text+'請小心。')
         line_bot_api.reply_message(event.reply_token, message)
-    elif Probability <=50:
+    elif Probability <=49:
         message = TextSendMessage(result_text+'請放心。')
         line_bot_api.reply_message(event.reply_token, message)
 
